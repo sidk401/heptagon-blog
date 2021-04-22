173f50611ccf482b5d250c8956c264211c5a0878
sidh <sidhkumarx@gmail.com>
2021-04-22 12:34:10 +0530

    Removed unwanted code

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
-use auth;
 
 class HomeController extends Controller
 {
@@ -26,23 +25,6 @@ class HomeController extends Controller
     public function index()
     {
 
-        $subscribed = auth::user()->is_subscribed;
-        $limit = env('PAGE_LIMIT');
-        $posts = ($subscribed ==1) ? Post::where('is_approved',1)->paginate($limit) : 
-                                     Post::where(['is_approved'=>1,'user_id'=>auth::user()->id])->paginate($limit);
-         dd($posts);
-       // return view('post.index',compact('posts'));
-
     }
 
-      
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
-    public function adminHome()
-    {
-        return view('adminHome');
-    }
 }
